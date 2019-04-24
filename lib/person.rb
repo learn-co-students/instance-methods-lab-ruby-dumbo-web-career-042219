f8fc7520337db5353ca1d1f0d072307d3e63c69f
Chloe <gingkolane@gmail.com>
2019-04-23 22:27:47 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+# #### 4. Define a `Person` in `lib/person.rb`
+#
+# Open `lib/person.rb` and add a class definition for a `Person` class.
+class Person
+# #### 5. Define `#talk` in `Person`
+  def talk
+    puts "Hello World!"
+  end
+# #### 6. Define `#walk` in `Person`
+  def walk
+  puts "The Person is walking"
+  end
+
+end
